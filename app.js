662e572b69608cab7584e988431089637494bc7e
ketanbgm <ketan@rorko.com>
2017-04-07 18:34:05 +0530

    config setup

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 var app = angular.module('app', []);
+
+app.service('Config', function($http) {
+  return function() {
+    return $http.get('config.json');
+  };
+});
--- controllers/weatherController.js
+++ controllers/weatherController.js
@@ -1,17 +1,21 @@
-app.controller('weatherController', function ($scope) {
+app.controller('weatherController', function ($scope, $http, Config) {
 
 $scope.init = function () {
 	console.log("inside init")
      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
+        navigator.geolocation.getCurrentPosition($scope.showPosition);
     } else { 
         console.log("Geolocation is not supported by this browser.");
     }
 };
 
 
-function showPosition(position) {
-    console.log("Latitude: " + position.coords.latitude + 
-    "<br>Longitude: " + position.coords.longitude);
+$scope.showPosition =function(position) {
+    console.log("Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude);
+    Config()
+    .then(function(config) {
+      //$http.get(config.apiUrl);
+      console.log(config.data.apiUrl);
+    });
 }
 })
