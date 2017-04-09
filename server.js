7ca4b7c8724b37a30b27a2e811330d94a3baa112
ketanbgm <ketanbgm@gmail.com>
2017-04-10 01:30:03 +0530

    setup

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var bodyparser = require('body-parser');
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(bodyparser.json());
 app.use(express.static(__dirname + '/public'));
-var server = app.listen(80, function() {
-    console.log('Server listening on port ' + server.address().port);
-})
+// var server = app.listen(80, function() {
+//     console.log('Server listening on port ' + server.address().port);
+// })
+app.listen(process.env.PORT || 8888)
