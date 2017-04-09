5bc7f6e467fda6a0914f8d705670788c4f6b09af
ketanbgm <ketanbgm@gmail.com>
2017-04-10 00:35:36 +0530

    node server setup

---
+++ server.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+var bodyparser = require('body-parser');
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.json());
+app.use(express.static(__dirname + '/public'));
+var server = app.listen(80, function() {
+    console.log('Server listening on port ' + server.address().port);
+})
